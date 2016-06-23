b33969a6115b3dda8a176ebaeab28d17b7da1d54
Sergey <jiamepok2008@yandex.ru>
2016-06-23 17:32:25 +0300

    Enter Hometask_2

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,15 +1,51 @@
 package com.softdesign.devintensive.ui.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import com.softdesign.devintensive.R;
+import com.softdesign.devintensive.utils.ConstantMenedger;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = ConstantMenedger.TUG + "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
     }
 }
+
+
---
+++ app/src/main/java/com/softdesign/devintensive/utils/ConstantMenedger.java
@@ -0,0 +1,9 @@
+package com.softdesign.devintensive.utils;
+
+/**
+ * Created by Сергей on 23.06.2016.
+ */
+public interface ConstantMenedger {
+    String TUG = "DEV ";
+    String COLOR_MODE_KEY = "COLOR_MODE_KEY";
+}
